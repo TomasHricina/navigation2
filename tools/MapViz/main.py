877ef0f545c67b2e4a8f61ebf6baeefa0192fc3d
TomasHricina <tomas.hricina314@gmail.com>
2021-02-22 19:01:59 +0100

    1.0.2

--- tools/MapViz/main.py
+++ tools/MapViz/main.py
@@ -1,16 +1,28 @@
-#!/usr/bin/python3
 
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
-import numpy as np
-import os
-import cv2
-import sys
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QDesktopWidget, QLineEdit, QSlider, QPushButton
+from PyQt5.QtWidgets import QGridLayout, QVBoxLayout, QHBoxLayout, QFileDialog
+from PyQt5.QtGui import QImage, QPixmap, QMouseEvent, QKeyEvent
+from PyQt5.QtCore import QPoint, QPointF, QRect, QRectF, QSize, QSizeF, pyqtSignal
+from PyQt5.Qt import Qt
+from canvas import ImageView
+import os, sys
 from collections import namedtuple
-from helpers import *
+from helpers import scaleToFit, parse_map_yaml
 
-# globals:
+
+#TODO: Drag and drop
+#TODO: Undo Redo - in progress
+
+__version__ = "1.1.0"
+__title__ = "MapViz"
+__uri__ = "https://github.com/TomasHricina/navigation2"
+__author__ = "Tomas Hričina"
+__maintainer__ = __author__
+__credits__ = ["Steve Macenski", ]
+__email__ = "t.hri@seznam.cz"
+__license__ = None
+
 available_resolution = None  # calculated in main()
 dimensions = namedtuple('dimensions', 'width height')
 load_button_dimensions = dimensions(width=80, height=80)
@@ -18,6 +30,34 @@ save_button_dimensions = dimensions(width=80, height=80)
 canvas_min_dim = dimensions(width=400, height=400)
 canvas_height_offset = 50
 
+
+class AppImageView(ImageView):
+    def __init__(self, *args, **kwargs):
+        ImageView.__init__(self, *args, **kwargs)
+
+    def updateStatusBar(self) -> None:
+        status_msg = 'ui: %d,%d | image: %d,%d | angle: %d | rotation_speed: %d | pan_speed: %d'\
+                     % (self.cursor_widget_x, self.cursor_widget_y,
+                        self.cursor_image_x, self.cursor_image_y,
+                        self.angle, self.rotating_speed, self.pan_speed)
+        self.main_widget.statusBar().showMessage(status_msg)
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        ImageView.mousePressEvent(self, event)
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        ImageView.mouseMoveEvent(self, event)
+        pos = event.pos()
+        self.scene_pos = self.mapToScene(pos)
+        self.cursor_widget_x, self.cursor_widget_y = pos.x(), pos.y()
+        self.cursor_image_x, self.cursor_image_y = round(self.scene_pos.x()), round(self.scene_pos.y())
+        self.updateStatusBar()
+
+
+    def keyPressEvent(self, event: QKeyEvent) -> None:
+        ImageView.keyPressEvent(self, event)
+        self.cursor_image_x, self.cursor_image_y = round(self.scene_pos.x()), round(self.scene_pos.y())
+        self.updateStatusBar()
 
 class LoadButton(QPushButton):
     def __init__(self, *args, **kwargs):
@@ -32,153 +72,78 @@ class SaveButton(QPushButton):
         super(SaveButton, self).__init__(*args, **kwargs)
         print('Save')
         self.setFixedSize(*save_button_dimensions)
-        self.setText("Save")
+        self.setText("Save As")
 
 
-class SizeSlider(QSlider):
-    def __init__(self, *args, **kwargs):
-        super(SizeSlider, self).__init__(*args, **kwargs)
-        self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        self.setOrientation(Qt.Horizontal)
-        self.setDisabled(True)
-
-
-class RotationSlider(QSlider):
-    def __init__(self, *args, **kwargs):
-        super(RotationSlider, self).__init__(*args, **kwargs)
-        self.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        self.setOrientation(Qt.Horizontal)
-        self.setRange(0, 360)
-        self.setDisabled(True)
-
-
-class LeftVerticalMenu(QWidget):
+class TopMenu(QWidget):
     def __init__(self):
         super().__init__()
-        print('LeftMenu')
+        print('TopMenu')
         self.load_button = LoadButton()
         self.save_button = SaveButton()
-        self.size_slider = SizeSlider()
-        self.rotation_slider = RotationSlider()
-        self.vbox = QVBoxLayout()
-        self.vbox.addWidget(self.load_button)
-        self.vbox.addWidget(self.save_button)
-        self.vbox.addWidget(self.size_slider)
-        self.vbox.addWidget(self.rotation_slider)
-        self.vbox.setAlignment(Qt.AlignTop)
-        self.setLayout(self.vbox)
+        self.hbox = QHBoxLayout()
+        self.hbox.addWidget(self.load_button)
+        self.hbox.addWidget(self.save_button)
+        self.hbox.setAlignment(Qt.AlignTop)
+        self.setLayout(self.hbox)
 
 
-class Canvas(QLabel):
-    def __init__(self):
-        super().__init__()
-        print('Canvas')
-        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.setStyleSheet("QLabel {background-color: red;}")
-        self.setAlignment(Qt.AlignLeft)
+class ImageViewerWindow(QMainWindow):
+    def __init__(self, image, input_path):
+        QMainWindow.__init__(self)
+        self.image = image
+        self.input_path = input_path
+        self.image_view = AppImageView(self)
+        self.image_view.main_widget = self
+        self.top_menu = TopMenu()
 
+        padding = self.frameGeometry().size() - self.geometry().size()
+        self.resize(image.size() + padding)
+        self.central = QWidget(self)
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.central)
+        self.image_view.setMouseTracking(True)
+        self.verticalLayout.addWidget(self.top_menu)
+        self.verticalLayout.addWidget(self.image_view)
 
-class Panel(QWidget):
-    def __init__(self, parent=None):
-        super(Panel, self).__init__(parent=None)
-        print('Panel')
-        self.panel_lay = QGridLayout(self)
-        self.left_menu = LeftVerticalMenu()
-        self.canvas = Canvas()
-        self.panel_lay.addWidget(self.left_menu, 0, 0)
-        self.panel_lay.addWidget(self.canvas, 0, 1)
+        self.setCentralWidget(self.central)
+        screen = QDesktopWidget().screenGeometry(self)
+        size = self.geometry()
+        self.move(int((screen.width()-size.width())/4), int((screen.height()-size.height())/4))
+        # self.setStyleSheet('background: red')
+        self.update_view()
+        self.image_view.reset()
 
+        self.top_menu.load_button.clicked.connect(self.load_yaml)
 
-class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent=parent)
-        print('MainWindow')
-        self.default_dim = dimensions(width=800, height=600)
-        self.default_window_x = 0
-        self.default_window_y = 0
-        self.angle = 0  # counter-clockwise
-        self.setGeometry(self.default_window_x, self.default_window_y, self.default_dim.width, self.default_dim.height)
-        self.initUi()
+    def update_view(self):
+        self.image_view.image = self.image
+        self.image_view.const_pixmap = QPixmap.fromImage(self.image)
+        self.image_view.pixmap_history.append(QPixmap.fromImage(self.image))     # this is for default image
+        self.setWindowTitle('MapViz  file: ' + self.make_window_title())
 
-    def initUi(self):
-        self.panel = Panel(self)
-        self.setCentralWidget(self.panel)
-
-        # defaults
-        self.left_menu_dim = dimensions(width=self.panel.left_menu.sizeHint().width(),
-                                        height=self.panel.left_menu.sizeHint().height())
-        self.canvas_max_dim = dimensions(width=available_resolution.width - self.left_menu_dim.width,
-                                         height=available_resolution.height - canvas_height_offset)
-
-        # bind sliders
-        self.panel.left_menu.size_slider.valueChanged.connect(self.resizeWithSlider)
-        self.panel.left_menu.rotation_slider.valueChanged.connect(self.rotateWithSlider)
-
-        # bind load button with load_yaml
-        self.panel.left_menu.load_button.clicked.connect(self.load_yaml)
-
-    def updateCanvas(self, updated_pixmap):
-        self.panel.canvas.setPixmap(updated_pixmap)
-        self.setFixedSize(self.panel.sizeHint())
-
-    def resizeCanvas(self, const_image, target_canvas_w, target_canvas_h):
-        temp_pixmap = const_image.scaled(target_canvas_w, target_canvas_h)
-        self.updateCanvas(temp_pixmap)
-
-    def resizeWithSlider(self):
-        # guaranteed to execute after Load (slider is locked)
-        slider_value = self.panel.left_menu.size_slider.value()
-        self.interp_canvas_width = int(np.interp(slider_value, (0, 99), [canvas_min_dim.width, self.new_image_dim.width]))
-        self.interp_canvas_height = int(self.interp_canvas_width / self.const_image_width_height_ratio)
-
-        # rotation is done again on const image so the quality of image does not degrade
-        rotated_pixmap = rotation(self.const_image, self.angle)
-        self.rotated_image_dim = scaleToFit(canvas_w=self.interp_canvas_width, canvas_h=self.interp_canvas_height,
-                                            image_w=rotated_pixmap.width(), image_h=rotated_pixmap.height())
-        self.resizeCanvas(rotated_pixmap, self.rotated_image_dim.width, self.rotated_image_dim.height)
-
-    def rotateWithSlider(self):
-        # guaranteed to execute after Load (slider is locked)
-        slider_value = self.panel.left_menu.rotation_slider.value()
-        self.angle = slider_value
-
-        rotated_pixmap = rotation(self.const_image, self.angle)
-        self.rotated_image_dim = scaleToFit(canvas_w=self.interp_canvas_width, canvas_h=self.interp_canvas_height,
-                                            image_w=rotated_pixmap.width(), image_h=rotated_pixmap.height())
-        self.resizeCanvas(rotated_pixmap, self.rotated_image_dim.width, self.rotated_image_dim.height)
+    def make_window_title(self):
+        return os.path.basename(self.input_path)
 
     def load_yaml(self):
         # TODO: drag and drop file
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         file_name, _ = QFileDialog.getOpenFileName(self, "Load YAML", "", "YAML(*.yaml)", options=options)
-        print(file_name)
 
         if file_name:
-            yaml_directory, yaml_name = os.path.split(file_name)
-            os.chdir(yaml_directory)
-            parsed_yaml = parse_map_yaml(yaml_name)
+            parsed_yaml = parse_map_yaml(file_name)
             if type(parsed_yaml) is list:
+                # TODO: Notify user about YAML parsing error
                 # error parsing branch
-                # TODO: Notify user about YAML parsing error via some widget
                 print(parsed_yaml)
             else:
-                self.const_image = QPixmap(parsed_yaml['image'])
-                self.const_image_dim = dimensions(width=self.const_image.width(), height=self.const_image.height())
-                self.const_image_width_height_ratio = self.const_image.width() / self.const_image.height()
+                # success parsing branch
 
-                # scale image so it maximally uses resolution, while keeping sides ratio
-                self.new_image_dim = scaleToFit(canvas_w=self.canvas_max_dim.width, canvas_h=self.canvas_max_dim.height,
-                                                image_w=self.const_image_dim.width, image_h=self.const_image_dim.height)
-
-                # update canvas
-                self.resizeCanvas(self.const_image, self.new_image_dim.width, self.new_image_dim.height)
-
-                # update sliders
-                self.panel.left_menu.size_slider.setValue(99)
-                self.panel.left_menu.size_slider.setEnabled(True)
-                self.panel.left_menu.rotation_slider.setValue(0)
-                self.panel.left_menu.rotation_slider.setEnabled(True)
+                self.const_pixmap = QPixmap(parsed_yaml['image'])
+                self.image_view.const_pixmap = self.const_pixmap
+                self.image_view.pixmap = self.const_pixmap
+                self.image_view.pixmap_history.append(self.const_pixmap)
+                self.image_view.angle = 0
 
 
 def main():
@@ -187,9 +152,12 @@ def main():
     available_screen = screen.availableGeometry()
     global available_resolution
     available_resolution = dimensions(available_screen.width(), available_screen.height())
-    w = MainWindow()
-    w.show()
-    sys.exit(app.exec_())
+    input_image = 'map.png'
+    image = QImage()
+    image.load(input_image)
+    window = ImageViewerWindow(image, input_image)
+    window.show()
+    app.exec_()
 
 
 if __name__ == '__main__':
