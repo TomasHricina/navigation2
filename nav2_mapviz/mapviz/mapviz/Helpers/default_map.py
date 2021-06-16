581d3123aee87cd88e5b72ebec7dfec821f8357d
TomasHricina <tomas.hricina314@gmail.com>
2021-06-16 15:35:12 +0200

    1.0.6

--- nav2_mapviz/mapviz/mapviz/Helpers/default_map.py
+++ nav2_mapviz/mapviz/mapviz/Helpers/default_map.py
@@ -7,18 +7,17 @@ import yaml  # pip pyaml
 from PyQt5.QtGui import QImage
 
 # Source files
-from mapViz.Helpers.magic_gui_numbers import default_image_name
-
+from mapviz.Helpers.magic_gui_numbers import default_image_name
+from mapviz.Helpers.helpers import correct_image_path
 
 def yaml_and_image():
     try:
-        with open('Images/default_yaml.yaml', 'r') as file:
+        with open(correct_image_path('default_yaml.yaml'), 'r') as file:
             _default_yaml = yaml.safe_load(file)
         _default_image = QImage()
-        _default_image.load('Images/'+default_image_name)
+        _default_image.load(correct_image_path(default_image_name))
         return _default_yaml, _default_image
     except FileNotFoundError:
         pass
 
 default_yaml, default_image = yaml_and_image()
-
